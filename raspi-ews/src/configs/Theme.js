74e15765e674885060cab7b56a446d557b704564
Kadinata <2396847+Kadinata@users.noreply.github.com>
2020-04-10 23:24:53 -0700

    Changing Theme to use Material-UI theme.

---
+++ raspi-ews/src/configs/Theme.js
@@ -0,0 +1,19 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const Theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: 'rgba(33, 33, 64, 0.7)',
+    },
+    background: {
+      paper: 'rgba(0, 0, 26, 0.35)',
+    },
+    divider: 'rgba(0, 0, 0, 0.12)',
+    text: {
+      primary: '#E9E9E9',
+    },
+  },
+});
+
+export default Theme;
