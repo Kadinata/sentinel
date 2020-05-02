aef01bf895e13cd85e1d91d36ea8fe42380b10c1
Kadinata <2396847+Kadinata@users.noreply.github.com>
2020-05-02 00:56:58 -0700

    Changing SSE handler to be an event emitter that emits active listener counts.

--- server/src/utils/sse_handler.js
+++ server/src/utils/sse_handler.js
@@ -2,13 +2,27 @@
 //  
 //===========================================================================
 'use-strict';
+const { EventEmitter } = require('events');
 
-class SSEHandler {
+/** 
+ * A class for managing server-sent event (SSE) listeners
+ */
+class SSEHandler extends EventEmitter {
 
   constructor() {
+    super();
+
+    /** @private Object to keep track of active listeners */
     this.clients = {};
   }
 
+  /**
+   * Handles a new request, configures the response header for SSE, 
+   * and adds the request to the list of active listeners.
+   * @param {object} req - HTTP request object from the client
+   * @param {object} res - HTTP response objcet for the client
+   * @param {function} next - Function to call the next express handler
+   */
   handleRequest(req, res, next) {
     const headers = {
       'Content-Type': 'text/event-stream',
@@ -21,14 +35,27 @@ class SSEHandler {
     this.addClient(req, res);
   }
 
+  /**
+   * Add a new SSE listener and assigns an ID to it. The listener will be auto-removed 
+   * from the list of active listeners when the client closes the connection. 
+   * Once added, the new active listener count will be emitted.
+   * @param {object} req - HTTP request object from the client
+   * @param {object} res - HTTP response objcet for the client
+   */
   addClient(req, res) {
     const client_id = Date.now();
     this.clients[`${client_id}`] = res;
     req.on('close', () => this.removeClient(client_id));
+    this._emit_client_change();
     // console.log(`SSE: Client Added: ${client_id}`);
     return client_id;
   }
 
+  /**
+   * Remove the client with the given ID from the list of listeners and emits the new 
+   * active listener count. Connection to the removed listener will be closed.
+   * @param {number} client_id - ID of the client to be removed
+   */
   removeClient(client_id) {
     client_id = `${client_id}`;
     const {
@@ -37,11 +64,22 @@ class SSEHandler {
     } = this.clients;
     removed.end();
     this.clients = clients;
+    this._emit_client_change();
     // console.log(`SSE: Client Removed: ${client_id}`);
   }
 
+  /**
+   * Send data to the clients
+   * @param {object} data - Data to push to the clients
+   */
   send(data) {
+    // console.log(`SSE: Sending data`);
     Object.values(this.clients).forEach(res => res.write(JSON.stringify(data)));
+  }
+
+  /** @private Emits the current count of active listeners */
+  _emit_client_change() {
+    this.emit('clientChange', Object.keys(this.clients).length);
   }
 }
 
