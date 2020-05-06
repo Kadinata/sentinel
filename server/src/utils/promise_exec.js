c3c7a49dc7b3e1a6da71ec024f907d3ef555aa2e
Kadinata <2396847+Kadinata@users.noreply.github.com>
2020-05-06 00:56:09 -0700

    Adding doc string to promise_exec

--- server/src/utils/promise_exec.js
+++ server/src/utils/promise_exec.js
@@ -4,6 +4,11 @@
 'use-strict';
 const exec = require('child_process').exec;
 
+/**
+ * Executes a shell command and returns the output as a promise.
+ * @param {string} command - Shell command to execute
+ * @return - Promise containing the output of the command
+ */
 module.exports = (command) => {
   return new Promise((resolve, reject) => {
     exec(command, (error, stdout, stderr) => {
