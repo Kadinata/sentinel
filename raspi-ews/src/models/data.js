dfe42006169cbe19b1f5e27fe9d83c8316b3fbe0
Kadinata <2396847+Kadinata@users.noreply.github.com>
2020-05-25 18:27:05 -0700

    Adding cpu usage field to the dummy data model.

--- raspi-ews/src/models/data.js
+++ raspi-ews/src/models/data.js
@@ -1,31 +1,52 @@
 const Data = {
   "os_info": {
     "hostname": "raspberrypi",
-    "host_ip": "192.168.1.8",
+    "host_ip": "192.168.1.6",
     "type": "Linux",
     "platform": "linux",
-    "uptime": 15094,
     "arch": "arm",
     "release": "4.19.58-v7+",
     "distribution": "Raspbian GNU/Linux 10 (buster)"
   },
   "cpu_info": {
-    "cpu_temp": 44.5,
-    "cpu_freq": 600,
-    "core_voltage": 1.2,
+    "cpu_temp": 49.4,
+    "cpu_freq": 1400,
+    "core_voltage": 1.35,
     "core_num": 4,
     "load_1": 0,
     "load_5": 0,
     "load_15": 0,
     "processor": "ARMv7 Processor rev 4 (v7l)"
   },
+  "cpu_usage": {
+    "interval": 10010,
+    "timestamp": 1590452844878,
+    "usages": [
+      [
+        0,
+        100000
+      ],
+      [
+        0,
+        100100
+      ],
+      [
+        0,
+        100100
+      ],
+      [
+        100,
+        100100
+      ]
+    ]
+  },
   "hdd_info": [
     {
       "fs": "/dev/root",
       "type": "ext4",
       "total": 14674,
-      "used": 1382,
-      "avail": 12664,
+      "used": 1387,
+      "avail": 12660,
       "percent": 10,
       "mount": "/"
     },
@@ -41,32 +62,32 @@ const Data = {
   ],
   "mem_info": {
     "total_mem": 451772416,
-    "free_mem": 155406336,
-    "percent": 0.3439925291941684
+    "free_mem": 207101952,
+    "percent": 0.4584209762820048
   },
   "netstats": [
     {
       "interface": "wlan0",
-      "ipaddr": "192.168.1.8",
+      "ipaddr": "192.168.1.6",
       "rx": {
-        "packets": "5127",
-        "bytes": "866973",
+        "packets": "2569",
+        "bytes": "518068",
         "error": "0",
         "dropped": "0"
       },
       "tx": {
-        "packets": "3939",
-        "bytes": "1007704",
+        "packets": "1822",
+        "bytes": "861357",
         "error": "0",
         "dropped": "0"
       }
     }
   ],
+  "uptime": 18398,
+  "localtime": "2020-05-26T01:27:32.370",
   "mqtt_broker": {
     "online": true
-  },
-  "uptime": 15094,
-  "localtime": "2020-01-27T12:34:56.680"
+  }
 };
 
 export default Data;
