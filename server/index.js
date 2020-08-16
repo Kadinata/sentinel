f732eb519a52ef3d79c741a073311bef8dd07a6c
Kadinata <2396847+Kadinata@users.noreply.github.com>
2020-08-16 12:30:35 -0500

    Adding body parser and passport.initialize() middlewares.

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const path = require('path');
 const apiRoutes = require('./src/api');
 const MqttService = require('./src/mqtt_service');
 const services = require('./src/services');
+const passport = require('passport');
 
 const app = express();
 const port = process.env.port || 3000;
@@ -16,7 +17,9 @@ mqttService.start();
 app.set('json spaces', 2);
 
 app.use(cors());
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/v1/', apiRoutes);
@@ -25,8 +28,16 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-services.init().then(() => {
+(async () => {
+  services.auth.config(passport);
+  await services.init();
   app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
   });
-});
+})();
+
+// services.init().then(() => {
+//   app.listen(port, () => {
+//     console.log(`Server is listening on port ${port}`);
+//   });
+// });
