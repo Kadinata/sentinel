9537f7119d5785e2469a317d9b711d033bde7eee
Kadinata <2396847+Kadinata@users.noreply.github.com>
2020-09-13 15:17:51 -0500

    Applying Auth data and navigation provider to the top level component

--- raspi-ews/src/App.js
+++ raspi-ews/src/App.js
@@ -1,10 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
 
 import {
   BrowserRouter as Router,
-  Route,
-  Switch,
 } from 'react-router-dom';
 
 import { makeStyles } from '@material-ui/core';
@@ -12,12 +10,10 @@ import { ThemeProvider } from '@material-ui/core';
 
 import Theme from './configs/Theme';
 
-import SystemInfo from './views/SystemInfo/SystemInfo';
-import TestView from './views/Test/Test';
-import LoginView from './views/Login/Login';
-import MainLayout from './layouts/main';
-import Routes from './routes';
-import Data from './models/data';
+import Routes from './routes/Routes';
+import AuthDataProvider from './auth/AuthProvider';
+import NavProvider from './layouts/NavProvider';
+import NavRoutes from './navRoutes';
 
 const useStyles = makeStyles((theme) => ({
   header: {
@@ -25,6 +21,10 @@ const useStyles = makeStyles((theme) => ({
     minHeight: '100vh',
     color: '#E9E9E9',
     fontSize: 'calc(10px + 2vmin)',
+  },
+  canvas: {
+    display: 'flex',
+    minHeight: '100vh',
     background: 'linear-gradient(to bottom right, #13547A, #80D0C7)',
   },
 }));
@@ -33,38 +33,17 @@ function App() {
 
   const classes = useStyles();
 
-  const [data, setData] = useState();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const endpoint = `/api/v1/sysinfo`;
-        const result = await (await fetch(endpoint)).json();
-        setData(result);
-      } catch {
-        setData(Data);
-      }
-    };
-    fetchData();
-  }, []);
-
   return (
     <ThemeProvider theme={Theme}>
-      <Router>
-        <MainLayout className={[classes.header]} routes={Routes}>
-          <Switch>
-            <Route path="/systems">
-              <SystemInfo data={data} />
-            </Route>
-            <Route path="/test">
-              <TestView />
-            </Route>
-            <Route path="/login">
-              <LoginView />
-            </Route>
-          </Switch>
-        </MainLayout>
-      </Router>
+      <div className={classes.canvas}>
+        <Router>
+          <AuthDataProvider>
+            <NavProvider routes={NavRoutes}>
+              <Routes />
+            </NavProvider>
+          </AuthDataProvider>
+        </Router>
+      </div>
     </ThemeProvider>
   );
 }
